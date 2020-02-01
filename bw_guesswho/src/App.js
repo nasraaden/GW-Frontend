428f8ee8e2b4f6097b29335e8b5a9f47cf6f7f75
darkwolfxj <hesitation.dotdot@gmail.com>
2020-02-01 16:34:49 -0500

    Finished the Log In page

--- bw_guesswho/src/App.js
+++ bw_guesswho/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login'
 import styled from 'styled-components';
+import {Route, Switch} from 'react-router-dom';
+
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 const H1 = styled.h1`
     color: white;
@@ -9,12 +12,24 @@ const H1 = styled.h1`
     font-family: 'Nunito', sans-serif;
     font-weight: 900;
 `
+const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    max-width: 100vw;
+`
+
 function App() {
   return (
-    <div className="App">
-      <H1>Guess Who</H1>
-      <Login />
-    </div>
+    <Div className="App">
+        <Switch>  
+            <Route path='/create-account' render={props => <SignUp {...props} />} />
+            <Route path='/' render={(props)=><Login {...props} />}/>
+        </Switch>
+    </Div>
+    
   );
 }
 
--- bw_guesswho/src/components/Login.js
+++ bw_guesswho/src/components/Login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 import * as yup from 'yup';
 import {useForm} from 'react-hook-form';
+import {NavLink} from 'react-router-dom';
 
 const Form = styled.form`
     display: flex;
@@ -9,36 +10,96 @@ const Form = styled.form`
     align-items: center;
 `
 const Input = styled.input`
-    margin: 3em;
-    width: 75%;
+    width: 100%;
     font-size: 20pt;
     text-align: center;
     background-color: white;
     color: #1B9BEA;
+    border-radius: 5px;
+    margin: 2% 0;
+    padding: 0;
+    postition: absolute;
+    left: 90%;
 `
 const Button = styled.button`
-    border-radius: 25px;
+    border-radius: 30px;
     height: 20%;
-    width: 50%;
+    width: 42%;
     padding: 15px;
-    background-color: lightblue;
+    color: white;
+    box-shadow: none;
+    border: none;
+    position: relative;
+    margin-top: 10%;
+    background-color: white;
+    color: #1B9BEA;
+    :disabled {
+        background-color: #AAB8C2;
+        color: white;
+    }
 `
 const Label = styled.label`
+    margin-bottom: -2%;
+    width: 100%;
     color: white;
+    text-align: left;
+    font-family: 'Roboto', sans-serif;
 `
 const Div = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: left;
+    position: relative;
+`
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+`
+const StyledDiv2 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 75%;
+`
+const H2 = styled.h2`
+    font-size: 20px;
+    color: white;
+    margin-bottom: 25%;
+`
+const P = styled.p`
+    color: white;
+    font-size: 10px;
+    margin-top: 20%;
+`
+const Span1 = styled.span`
+    font-family: 'Londrina Outline', cursive;
+    color: white;
+    font-size: 24px;
+    margin-right: 1px;
+`
+const Span2 = styled.span`
+    font-family: 'Roboto', sans-serif;
+    color: white;
+    font-size: 20px;
+`
+const Div2 = styled.div`
+    display: flex;
+    align-items: baseline;
+    position: absolute;
+    top: 5%;
 `
 
 const validationSchema = yup.object().shape({
-    name: yup
-    .string().required('You need a username.')
-    .min(3,'You need a longer username.')
-    .max(12, 'Your username is too long.'),
+    email: yup
+    .string().required('Enter an email.')
+    .min(3,'You need a longer email.')
+    .max(36, 'Your email is too long.'),
     password: yup
-    .string().required('You need a password.')
+    .string().required('Enter a password.')
     .min(6, 'Password is too short.')
     .max(16, 'Password exceeds character limit.')
 });
@@ -46,37 +107,43 @@ const validationSchema = yup.object().shape({
 
 export default function Login() {
     const {register, handleSubmit, errors} = useForm({validationSchema: validationSchema});
-    const [error, setError] = useState({name:{message: '', type: ''}, password:{message:'', type:''}})
-    const onSubmit = () => {console.log('This is error.name')};
+    const onSubmit = () => {document.getElementById('form').reset()};
     return (
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Div>
-                    <Label htmlFor='username'>Username</Label>
-                    <Input name='username' type='text'  ref={register} />
-                </Div>
-                <Div>
-                <Label htmlFor='name'>Password</Label>
-                <Input name='password' type='password' ref={register} />
-                </Div>
-                <div style={{color: 'red'}}>
-                    <pre>
-                        { errors.name !== undefined && errors.password !== undefined && (
-                            <>
-                                <p><label>{Object.values(errors.name)[0]}</label></p>
-                                <p><label>{Object.values(errors.password)[0]}</label></p>
-                            </>
-                        )|| errors.password !== undefined && (
-                            <>
-                                <p><label>{Object.values(errors.password)[0]}</label></p>
-                            </>
-                        )|| errors.name !== undefined && (
-                            <>
-                                <p><label>{Object.values(errors.name)[0]}</label></p>
-                            </>
-                        )}
-                    </pre>
-                </div>
-                <Button type='submit'>Log In</Button>
+            <Form id='form' onSubmit={handleSubmit(onSubmit)}>
+                <StyledDiv>
+                    <Div2><Span1>Guess</Span1><Span2>Who?</Span2></Div2>
+                    <H2>Sign In</H2>
+                    <StyledDiv2>
+                        <Div>
+                            <Label htmlFor='email'>Email</Label>
+                            <Input name='email' type='email' ref={register} />
+                        </Div>
+                        <Div>
+                            <Label htmlFor='name'>Password</Label>
+                            <Input name='password' type='password' ref={register} />
+                        </Div>
+                    </StyledDiv2>
+                    <Div>
+                        <pre>
+                            { errors.email !== undefined && errors.password !== undefined && (
+                                <>
+                                    <p><Label>{Object.values(errors.email)[0]}</Label></p>
+                                    <p><Label>{Object.values(errors.password)[0]}</Label></p>
+                                </>
+                            )|| errors.password !== undefined && (
+                                <>
+                                    <p><Label>{Object.values(errors.password)[0]}</Label></p>
+                                </>
+                            )|| errors.email !== undefined && (
+                                <>
+                                    <p><Label>{Object.values(errors.email)[0]}</Label></p>
+                                </>
+                            )}
+                        </pre>
+                    </Div>
+                    <Button type='submit'>Sign In</Button>
+                    <P>Don't have an account? <NavLink to='/create-account'>Create Account</NavLink> </P>
+                </StyledDiv>
         </Form>
     )
 }
