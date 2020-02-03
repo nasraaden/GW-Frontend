cd5cf17ad10fd2c5533587168f5e14b0baa9850e
aaronjan <aaronjan98@gmail.com>
2020-02-03 09:34:43 -0800

    added nice wallpaper

--- bw_guesswho/src/App.js
+++ bw_guesswho/src/App.js
@@ -1,33 +1,24 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './styles/App.css';
-<<<<<<< HEAD
-import { H1, AppDiv} from './styles/Styles';
-=======
-import {AppDiv} from './styles/Styles';
->>>>>>> origin/thomas-shottsjr
+import { AppDiv } from './styles/Styles';
 
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import PrivateRoute from './components/PrivateRoute';
 import HomePage from './components/home-page/HomePage';
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/thomas-shottsjr
 function App() {
-  return (
-    <AppDiv className="App">
-        <Switch>  
-            <PrivateRoute exact path="/home-page" component={HomePage} />
-            <Route path='/create-account' render={props => <SignUp {...props} />} />
-            <Route path='/' render={(props)=><Login {...props} />}/>
-        </Switch>
-    </AppDiv>
-    
-  );
+	return (
+		<AppDiv className="App">
+			<Switch>
+				<PrivateRoute exact path="/home-page" component={HomePage} />
+				<Route path="/create-account" render={(props) => <SignUp {...props} />} />
+				<Route path="/" render={(props) => <Login {...props} />} />
+			</Switch>
+		</AppDiv>
+	);
 }
 
 export default App;
-
