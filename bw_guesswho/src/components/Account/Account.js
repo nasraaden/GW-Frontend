1fa7defa8fc20ad94d2933b8b9b549bfafa791aa
darkwolfxj <hesitation.dotdot@gmail.com>
2020-02-05 11:46:33 -0500

    modified account component

--- bw_guesswho/src/components/Account/Account.js
+++ bw_guesswho/src/components/Account/Account.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Account() {
     return (
         <div>
-            <h1>Username</h1>
+            <h1>Email</h1>
             <h1>Score</h1>
             <h1>Wrong Answers</h1>
         </div>
