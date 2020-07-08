bef44fb3cb1fcb8e1f8938c59e0a5ca138265cd3
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-07-08 14:40:59 -0700

    changed styling to be responsive on the desktop, homepage

--- bw_guesswho/src/App.js
+++ bw_guesswho/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './styles/App.css';
-import { AppDiv, Div2, Span1, Span2 } from './styles/Styles';
+import { AppDiv, Div2, Span1, Span2, H2 } from './styles/Styles';
 
 import Login from './components/Login';
 import SignUp from './components/SignUp';
@@ -12,22 +12,22 @@ import Game from './components/home-page/game/Game';
 import Account from './components/Account/Account';
 import Settings from './components/Settings/Settings';
 
-
 function App() {
-	
-	return (
-		<AppDiv className="App">
-			<Div2><Span1>Guess</Span1><Span2>Who?</Span2></Div2>
-			<Switch>
-				<PrivateRoute exact path="/home-page" component={HomePage} />
-                <Route path="/account" component={Account} />
-				<Route path="/settings" render={props => <Settings {...props} />} />
-				<Route exact path="/game" render={props => <Game  {...props}/>}/>
-				<Route path="/create-account" render={(props) => <SignUp {...props} />} />
-				<Route path="/" render={(props) => <Login {...props} />} />
-			</Switch>
-		</AppDiv>
-	);
+  return (
+    <AppDiv className='App'>
+      <Switch>
+        <PrivateRoute exact path='/home-page' component={HomePage} />
+        <Route path='/account' component={Account} />
+        <Route path='/settings' render={(props) => <Settings {...props} />} />
+        <Route exact path='/game' render={(props) => <Game {...props} />} />
+        <Route
+          path='/create-account'
+          render={(props) => <SignUp {...props} />}
+        />
+        <Route path='/' render={(props) => <Login {...props} />} />
+      </Switch>
+    </AppDiv>
+  );
 }
 
 export default App;
--- bw_guesswho/src/styles/Styles.js
+++ bw_guesswho/src/styles/Styles.js
@@ -2,335 +2,347 @@ import styled from 'styled-components';
 
 /* App.js Styling */
 export const H1 = styled.h1`
-	color: white;
-	font-size: 24px;
-	font-family: 'Nunito', sans-serif;
-	font-weight: 900;
+  color: white;
+  font-size: 24px;
+  font-family: 'Nunito', sans-serif;
+  font-weight: 900;
 `;
 export const AppDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	height: 100vh;
-	max-width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  max-width: 100vw;
 `;
 
 /* Sign Up and Login styles below */
 export const Form = styled.form`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 export const Input = styled.input`
-	width: 100%;
-	font-size: 20pt;
-	text-align: center;
-	background-color: white;
-	color: #1b9bea;
-	border-radius: 5px;
-	margin: 2% 0;
-	padding: 0;
-	postition: absolute;
-	left: 90%;
+  width: 24rem;
+  padding: 0.8rem 0;
+  text-align: center;
+  background-color: white;
+  color: #1b9bea;
+  border-radius: 0.5rem;
+  margin: 2% 0;
+  postition: absolute;
+  left: 90%;
 `;
 export const Button = styled.button`
-	border-radius: 30px;
-	height: 10%;
-	width: 30%;
-	padding: 10px 0px;
-	color: white;
-	box-shadow: none;
-	border: none;
-	position: relative;
-	margin-top: 10%;
-	background-color: white;
-	color: #1b9bea;
-	:disabled {
-		background-color: #aab8c2;
-		color: white;
-	}
+  border-radius: 0.5rem;
+  width: 24rem;
+  padding: 0.8rem;
+  color: white;
+  cursor: pointer;
+  border: none;
+  position: relative;
+  margin: 1rem;
+  background-color: white;
+  color: #1b9bea;
+  :disabled {
+    background-color: #aab8c2;
+    color: white;
+  }
 `;
 export const Label = styled.label`
-	margin-bottom: -2%;
-	width: 100%;
-	color: white;
-	text-align: left;
-	font-family: 'Roboto', sans-serif;
+  margin-bottom: -2%;
+  width: 100%;
+  color: white;
+  text-align: left;
+  font-family: 'Roboto', sans-serif;
 `;
 export const Div = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: left;
-	position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  position: relative;
 `;
 export const StyledDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 export const StyledDiv2 = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 75%;
 `;
 export const H2 = styled.h2`
-	font-size: 20px;
-	color: white;
-	margin-bottom: 25%;
+  font-size: 2rem;
+  color: white;
 `;
 export const P = styled.p`
-	color: white;
-	font-size: 10px;
-	margin-top: 20%;
+  color: white;
+  font-size: 1rem;
+  margin-top: 2rem;
 `;
 export const Span1 = styled.span`
-	font-family: 'Londrina Outline', cursive;
-	color: white;
-	font-size: 24px;
-	margin-right: 1px;
+  font-family: 'Londrina Outline', cursive;
+  color: white;
+  font-size: 2rem;
+  margin-right: 1px;
+  border: 2px solid pink;
+  text-align: center;
 `;
 export const Span2 = styled.span`
-	font-family: 'Roboto', sans-serif;
-	color: white;
-	font-size: 20px;
+  font-family: 'Roboto', sans-serif;
+  color: white;
+  font-size: 2rem;
+  border: 2px solid purple;
+  text-align: center;
 `;
 
 export const StyledP = styled.p`
-    margin: 0;
+  margin: 0;
 `;
 
 export const Img = styled.img`
-	height: 40px;
-	width: 40px;
+  height: 40px;
+  width: 40px;
 
-	font-family: 'Roboto', sans-serif;
-	color: white;
-	font-size: 20px;
-`
+  font-family: 'Roboto', sans-serif;
+  color: white;
+  font-size: 20px;
+`;
 export const Span3 = styled.span`
-    position: absolute;
-    top: 5%;
-    left: 5%;
-`
+  position: absolute;
+  top: 5%;
+  left: 5%;
+`;
 export const Div2 = styled.div`
-display: flex;
-align-items: baseline;
-position: absolute;
-top: 4%;
-left: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 // Menu styling
 export const MenuDiv = styled.div`
-    position: fixed;
-    left: 85%;
-	top: 10%;
-	z-index: 2;
-`
+  position: fixed;
+  left: 85%;
+  top: 10%;
+  z-index: 2;
+`;
+export const MenuDiv2 = styled.div`
+  display: flex;
+  border: 2px solid red;
+`;
 export const ShowingDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: fixed;
-    left: 65%;
-    top: 14%;
-    background-color: #FFFFFF;
-    padding: 2% 5%;
-    border-radius: 5px;
-`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  left: 65%;
+  top: 14%;
+  background-color: #ffffff;
+  padding: 2% 5%;
+  border-radius: 0.5rem;
+`;
 //HomePage styles
 export const LongButton = styled.button`
-	width: 55%;
-	border-radius: 30px;
-	height: 20%;
-	padding: 15px;
-	color: white;
-	box-shadow: none;
-	border: none;
-	position: relative;
-	margin: 1% 0;
-	background-color: white;
-	background-color: #aab8c2;
-	color: white;
-`
+  width: 23rem;
+  border-radius: 2.5rem;
+  height: 20%;
+  padding: 1rem;
+  color: white;
+  box-shadow: none;
+  border: none;
+  position: relative;
+  margin: 1% 0;
+  background-color: white;
+  background-color: #aab8c2;
+  color: white;
+  border: 2px solid red;
+`;
 export const PresButton = styled.button`
-	width: 55%;
-	border-radius: 30px;
-	height: 20%;
-	padding: 15px;
-	box-shadow: none;
-	border: none;
-	position: relative;
-	margin: 1% 0;
-	background-color: white;
-	color: #1B9BEA;
-`
+  width: 23rem;
+  border-radius: 2.5rem;
+  height: 20%;
+  padding: 1rem;
+  box-shadow: none;
+  border: none;
+  position: relative;
+  margin: 1% 0;
+  background-color: white;
+  color: #1b9bea;
+`;
 export const StyledButton = styled.button`
-	width: 50%;
-	border-radius: 30px;
-	height: 20%;
-	padding: 15px;
-	color: white;
-	box-shadow: none;
-	border: none;
-	position: relative;
-	margin: 1% 0;
-	background-color: white;
-	color: #1b9bea;
-	:disabled {
-		background-color: #aab8c2;
-		color: white;
-	}
-`
+  width: 23rem;
+  border-radius: 3rem;
+  height: 20%;
+  padding: 1rem 0;
+  color: white;
+  box-shadow: none;
+  border: none;
+  position: relative;
+  margin: 1rem;
+  background-color: white;
+  color: #1b9bea;
+  :disabled {
+    background-color: #aab8c2;
+    color: white;
+  }
+`;
+export const PlayButton = styled.button`
+  width: 10rem;
+  border-radius: 3rem;
+  padding: 1rem 0;
+  color: #1b9bea;
+  margin-top: 2rem;
+  border: none;
+  cursor: pointer;
+`;
 export const StyledButton2 = styled.button`
-	width: 50%;
-	border-radius: 30px;
-	height: 20%;
-	padding: 15px;
-	color: white;
-	box-shadow: none;
-	border: none;
-	position: relative;
-	margin: 0;
-	background-color: white;
-	color: #1b9bea;
-	border-right: 1px solid;
-	border-left: none;
-	border-top: none;
-	border-bottom: none;
-	background-color: #aab8c2;
-	color: white;
-`
+  width: 23rem;
+  border-radius: 30px;
+  height: 20%;
+  padding: 15px;
+  color: white;
+  box-shadow: none;
+  border: none;
+  position: relative;
+  margin: 0;
+  background-color: white;
+  color: #1b9bea;
+  border-right: 1px solid;
+  border-left: none;
+  border-top: none;
+  border-bottom: none;
+  background-color: #aab8c2;
+  color: white;
+`;
 //Game Styles
 export const Choice = styled.button`
-	color: white;
-	font-size: 1.1rem;
-	width: 100%;
-	border-radius: 30px;
-	height: 20%;
-	padding: 5% 15%;
-	color: white;
-	box-shadow: none;
-	border: none;
-	margin: 2% 0%;
-	background-color: white;
-	color: #1b9bea;
-	:active {
-		background: #1b9bea;
-		color: white;
-	}
-	display: flex;
-	justify-content: space-evenly;
-	align-items: center;
-`
+  color: white;
+  font-size: 1.1rem;
+  width: 100%;
+  border-radius: 30px;
+  height: 20%;
+  padding: 5% 15%;
+  color: white;
+  box-shadow: none;
+  border: none;
+  margin: 2% 0%;
+  background-color: white;
+  color: #1b9bea;
+  :active {
+    background: #1b9bea;
+    color: white;
+  }
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 export const ChoiceDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	width: 50%;
-	justify-content: center;
-	position: absolute;
-	left: 25%;
-`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  justify-content: center;
+  position: absolute;
+  left: 25%;
+`;
 export const Submit = styled.button`
-	color: white;
-	width: 50%;
-	border-radius: 30px;
-	height: 20%;
-	padding: 15px;
-	color: white;
-	box-shadow: none;
-	border: none;
-	margin: 2% 0%;
-	background-color: white;
-	color: #1b9bea;
-	:active {
-		background: #1b9bea;
-		color: white;
-	}
-`
+  color: white;
+  width: 50%;
+  border-radius: 30px;
+  height: 20%;
+  padding: 15px;
+  color: white;
+  box-shadow: none;
+  border: none;
+  margin: 2% 0%;
+  background-color: white;
+  color: #1b9bea;
+  :active {
+    background: #1b9bea;
+    color: white;
+  }
+`;
 export const ScoreP = styled.p`
-    color: white;
-    font-size: 2rem;
-`
+  color: white;
+  font-size: 2rem;
+`;
 export const StyledTweet = styled.p`
-    position: relative;
-    top: 0%;
-    color: white;
-    text-align: left;
-    margin: 1em;
-	box-shadow: 5px 5px rgba(0, 0, 0, 25%);
-	border: 2px solid white;
-	padding: 2%;
-`
+  position: relative;
+  top: 0%;
+  color: white;
+  text-align: left;
+  margin: 1em;
+  box-shadow: 5px 5px rgba(0, 0, 0, 25%);
+  border: 2px solid white;
+  padding: 2%;
+`;
 export const TweetDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-content: space-between;
-`
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+`;
 export const ImageDiv = styled.div`
-    position: absolute;
-    top: 20%;
-    display: flex;
-    justify-content: space-evenly;
-`
+  position: absolute;
+  top: 20%;
+  display: flex;
+  justify-content: space-evenly;
+`;
 export const Cand = styled.img`
-    width: 20%;
-    border-radius: 30px;
-`
+  width: 20%;
+  border-radius: 30px;
+`;
 export const LevelP = styled.p`
-	color: white;
-	font-size: 2rem;
-	margin-left: 0%;
-`
+  color: white;
+  font-size: 2rem;
+  margin-left: 0%;
+`;
 export const InfoDiv = styled.div`
-	position: absolute;
-	top: 10%;
-	display: flex;
-	justify-content: space-evenly;
-	width: 100%;
-	height: 10%;
-`
+  position: absolute;
+  top: 10%;
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 10%;
+`;
 export const GameDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
 export const PresCandImg = styled.img`
-	height: 30px;
-	width: 30px;
-	border-radius: 50px;
-`
+  height: 30px;
+  width: 30px;
+  border-radius: 50px;
+`;
 //Setting Styles
 export const DeleteButton = styled.button`
-	padding: 10%;
-	color: white;
-	font-size: 3rem;
-	background-color: red;
-	border: none;
-	border-radius: 15px;
-	:active {
-		color: red;
-		background-color: white;
-	}
-`
+  padding: 10%;
+  color: white;
+  font-size: 3rem;
+  background-color: red;
+  border: none;
+  border-radius: 15px;
+  :active {
+    color: red;
+    background-color: white;
+  }
+`;
 export const DeleteDiv = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`;
 export const DeleteP = styled.p`
-	color: white;
-	font-size: 1.5rem;
-`
+  color: white;
+  font-size: 1.5rem;
+`;
 //Account Styles
 export const AccountDiv = styled.div`
-	color: white;
-	border: 2px solid white;
-	box-shadow: 5px 5px rgba(0, 0, 0, 25%);
-	padding: 2%;
-`
+  color: white;
+  border: 2px solid white;
+  box-shadow: 5px 5px rgba(0, 0, 0, 25%);
+  padding: 2%;
+`;
